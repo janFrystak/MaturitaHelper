28650d0d6a2e52cbc1256aa720b290460c9cdc17
janFrystak <janfrystak@gmail.com>
2026-05-09 13:39:22 +0200

    Add scrollbar styling to all notes; add EKO 07-08, IT 04 to manifest

--- manifest.js
+++ manifest.js
@@ -11,12 +11,15 @@ window.MAT_MANIFEST = {
     "04_Pravni_formy.html",
     "05_Personalni_cinnost.html",
     "06_Dlouhodoby_majetek.html",
+    "07_Pracovnepravni_vztahy.html",
+    "08_Financovani.html",
   ],
 
   it: [
     "01_Hardware.html",
     "02_Periferie.html",
     "03_Kancelársky_software.html",
+    "04_Algoritmizace.html",
   ],
 
   lit: [
